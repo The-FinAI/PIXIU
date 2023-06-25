89acac251474e89441adb5c076c925b9fe2531a3
黄济民 <jimin@chancefocus.com>
2023-06-25 16:51:11 -0400

    feature: add lm_eval based auto evaluation

---
+++ tasks/__init__.py
@@ -0,0 +1,87 @@
+from pprint import pprint
+from typing import List, Union
+
+import sacrebleu
+import lm_eval.base
+
+from . import flare
+
+TASK_REGISTRY = {
+    "flare_fpb": flare.FPB,
+    "flare_fiqasa": flare.FIQASA,
+    "flare_ner": flare.NER,
+    "flare_finqa": flare.FinQA,
+    **flare.SM_TASKS,
+}
+
+ALL_TASKS = sorted(list(TASK_REGISTRY))
+
+_EXAMPLE_JSON_PATH = "split:key:/absolute/path/to/data.json"
+
+
+def add_json_task(task_name):
+    """Add a JSON perplexity task if the given task name matches the
+    JSON task specification.
+
+    See `json.JsonPerplexity`.
+    """
+    if not task_name.startswith("json"):
+        return
+
+    def create_json_task():
+        splits = task_name.split("=", 1)
+        if len(splits) != 2 or not splits[1]:
+            raise ValueError(
+                "json tasks need a path argument pointing to the local "
+                "dataset, specified like this: json="
+                + _EXAMPLE_JSON_PATH
+                + ' (if there are no splits, use "train")'
+            )
+
+        json_path = splits[1]
+        if json_path == _EXAMPLE_JSON_PATH:
+            raise ValueError(
+                "please do not copy the example path directly, but substitute "
+                "it with a path to your local dataset"
+            )
+        return lambda: json.JsonPerplexity(json_path)
+
+    TASK_REGISTRY[task_name] = create_json_task()
+
+
+def get_task(task_name):
+    try:
+        add_json_task(task_name)
+        return TASK_REGISTRY[task_name]
+    except KeyError:
+        print("Available tasks:")
+        pprint(TASK_REGISTRY)
+        raise KeyError(f"Missing task {task_name}")
+
+
+def get_task_name_from_object(task_object):
+    for name, class_ in TASK_REGISTRY.items():
+        if class_ is task_object:
+            return name
+
+    # this gives a mechanism for non-registered tasks to have a custom name anyways when reporting
+    return (
+        task_object.EVAL_HARNESS_NAME
+        if hasattr(task_object, "EVAL_HARNESS_NAME")
+        else type(task_object).__name__
+    )
+
+
+def get_task_dict(task_name_list: List[Union[str, lm_eval.base.Task]]):
+    task_name_dict = {
+        task_name: get_task(task_name)()
+        for task_name in task_name_list
+        if isinstance(task_name, str)
+    }
+    task_name_from_object_dict = {
+        get_task_name_from_object(task_object): task_object
+        for task_object in task_name_list
+        if not isinstance(task_object, str)
+    }
+    assert set(task_name_dict.keys()).isdisjoint(set(task_name_from_object_dict.keys()))
+    return {**task_name_dict, **task_name_from_object_dict}
---
+++ tasks/flare.py
@@ -0,0 +1,396 @@
+"""
+FLARE
+"""
+from itertools import zip_longest
+from lm_eval.base import MultipleChoiceTask, Task, rf
+from lm_eval.metrics import mean, matthews_corrcoef
+import numpy as np
+from .utils import process_text
+from seqeval.metrics import f1_score as entity_score
+from sklearn.metrics import accuracy_score, f1_score
+
+
+_CITATION = """
+@misc{xie2023pixiu,
+      title={PIXIU: A Large Language Model, Instruction Data and Evaluation Benchmark for Finance}, 
+      author={Qianqian Xie and Weiguang Han and Xiao Zhang and Yanzhao Lai and Min Peng and Alejandro Lopez-Lira and Jimin Huang},
+      year={2023},
+      eprint={2306.05443},
+      archivePrefix={arXiv},
+      primaryClass={cs.CL}
+}
+"""
+
+
+class FPB(MultipleChoiceTask):
+    VERSION = 1
+    DATASET_PATH = "chancefocus/flare-fpb"
+    DATASET_NAME = None
+
+    def has_training_docs(self):
+        return True
+
+    def has_validation_docs(self):
+        return True
+
+    def has_test_docs(self):
+        return True
+
+    def training_docs(self):
+        return self.dataset["train"]
+
+    def validation_docs(self):
+        return self.dataset["validation"]
+
+    def test_docs(self):
+        return self.dataset["test"]
+
+    def _process_doc(self, doc):
+        # TODO: Process the documents into a dictionary with the following keys:
+        return {
+            "query": doc["question"],  # The query prompt.
+            "choices": doc["options"],  # The list of choices.
+            "gold": doc["gold"],  # The integer used to index into the correct element of `"choices"`.
+        }
+
+    def should_decontaminate(self):
+         return True
+
+    def doc_to_decontamination_query(self, doc):
+         return doc["text"]
+
+    def doc_to_text(self, doc):
+        # TODO: Format the query prompt portion of the document example.
+        return doc["query"]
+
+    def process_results(self, doc, results):
+        gold = doc["gold"]
+
+        acc = 1.0 if np.argmax(results) == gold else 0.0
+        completion_len = np.array([float(len(i)) for i in doc["choices"]])
+        acc_norm = 1.0 if np.argmax(results / completion_len) == gold else 0.0
+
+        return {
+            "acc": acc,
+            "acc_norm": acc_norm,
+            "f1": (np.argmax(results), gold),
+        }
+
+    def higher_is_better(self):
+        return {
+            "acc": True,
+            "acc_norm": True,
+            "f1": True,
+        }
+
+    def weighted_fi(cls, items):
+        preds, golds = zip(*items)
+        preds = np.array(preds)
+        golds = np.array(golds)
+        f1 = f1_score(preds, golds, average='weighted', labels=[0, 1, 2])
+        return f1
+
+    def aggregation(self):
+        return {
+            "acc": mean,
+            "acc_norm": mean,
+            "f1": self.weighted_fi,
+        }
+
+
+class FIQASA(MultipleChoiceTask):
+    VERSION = 1
+    DATASET_PATH = "chancefocus/flare-fiqasa"
+    DATASET_NAME = None
+
+    def has_training_docs(self):
+        return True
+
+    def has_validation_docs(self):
+        return True
+
+    def has_test_docs(self):
+        return True
+
+    def training_docs(self):
+        return self.dataset["train"]
+
+    def validation_docs(self):
+        return self.dataset["validation"]
+
+    def test_docs(self):
+        return self.dataset["test"]
+
+    def _process_doc(self, doc):
+        # TODO: Process the documents into a dictionary with the following keys:
+        return {
+            "query": doc["question"],  # The query prompt.
+            "choices": doc["options"],  # The list of choices.
+            "gold": doc["gold"],  # The integer used to index into the correct element of `"choices"`.
+        }
+
+    def should_decontaminate(self):
+         return True
+
+    def doc_to_decontamination_query(self, doc):
+         return doc["text"]
+
+    def doc_to_text(self, doc):
+        # TODO: Format the query prompt portion of the document example.
+        return doc["query"]
+
+    def process_results(self, doc, results):
+        gold = doc["gold"]
+
+        acc = 1.0 if np.argmax(results) == gold else 0.0
+        completion_len = np.array([float(len(i)) for i in doc["choices"]])
+        acc_norm = 1.0 if np.argmax(results / completion_len) == gold else 0.0
+
+        return {
+            "acc": acc,
+            "acc_norm": acc_norm,
+            "f1": (np.argmax(results), gold),
+        }
+
+    def higher_is_better(self):
+        return {
+            "acc": True,
+            "acc_norm": True,
+            "f1": True,
+        }
+
+    def weighted_fi(cls, items):
+        preds, golds = zip(*items)
+        preds = np.array(preds)
+        golds = np.array(golds)
+        f1 = f1_score(preds, golds, average='weighted', labels=[0, 1, 2])
+        return f1
+
+    def aggregation(self):
+        return {
+            "acc": mean,
+            "acc_norm": mean,
+            "f1": self.weighted_fi,
+        }
+
+
+class NER(Task):
+    VERSION = 1
+    DATASET_PATH = "chancefocus/flare-ner"
+    DATASET_NAME = None
+
+    def has_training_docs(self):
+        return True
+
+    def has_validation_docs(self):
+        return True
+
+    def has_test_docs(self):
+        return True
+
+    def training_docs(self):
+        return self.dataset["train"]
+
+    def validation_docs(self):
+        return self.dataset["validation"]
+
+    def test_docs(self):
+        return self.dataset["test"]
+
+    def should_decontaminate(self):
+         return True
+
+    def doc_to_decontamination_query(self, doc):
+         return doc["text"]
+
+    def doc_to_text(self, doc):
+        # TODO: Format the query prompt portion of the document example.
+        return doc["query"]
+
+    def construct_requests(self, doc, ctx):
+        """Uses RequestFactory to construct Requests and returns an iterable of
+        Requests which will be sent to the LM.
+
+        :param doc:
+            The document as returned from training_docs, validation_docs, or test_docs.
+        :param ctx: str
+            The context string, generated by fewshot_context. This includes the natural
+            language description, as well as the few shot examples, and the question
+            part of the document for `doc`.
+        """
+        cont_request = rf.greedy_until(ctx, {"until": "\n\n"})
+        return cont_request
+
+    def doc_to_target(self, doc):
+        return doc["answer"]
+
+    def process_results(self, doc, results):
+        text = doc["text"]
+        pred = process_text(results[0], text)
+
+        return {
+            "entity_f1": (pred, doc["label"], results[0])
+        }
+
+    def higher_is_better(self):
+        return {
+            "entity_f1": True,
+        }
+
+    @classmethod
+    def entity_f1(cls, items):
+        preds, golds, _ = zip(*items)
+        f1 = entity_score(preds, golds)
+        return f1
+
+    def aggregation(self):
+        return {
+            "entity_f1": self.entity_f1,
+        }
+
+
+class FinQA(Task):
+    VERSION = 1
+    DATASET_PATH = "chancefocus/flare-finqa"
+    DATASET_NAME = None
+
+    def has_training_docs(self):
+        return True
+
+    def has_validation_docs(self):
+        return True
+
+    def has_test_docs(self):
+        return True
+
+    def training_docs(self):
+        return self.dataset["train"]
+
+    def validation_docs(self):
+        return self.dataset["validation"]
+
+    def test_docs(self):
+        return self.dataset["test"]
+
+    def should_decontaminate(self):
+         return True
+
+    def doc_to_decontamination_query(self, doc):
+         return doc["text"]
+
+    def doc_to_text(self, doc):
+        # TODO: Format the query prompt portion of the document example.
+        return doc["query"]
+
+    def construct_requests(self, doc, ctx):
+        """Uses RequestFactory to construct Requests and returns an iterable of
+        Requests which will be sent to the LM.
+
+        :param doc:
+            The document as returned from training_docs, validation_docs, or test_docs.
+        :param ctx: str
+            The context string, generated by fewshot_context. This includes the natural
+            language description, as well as the few shot examples, and the question
+            part of the document for `doc`.
+        """
+        cont_request = rf.greedy_until(ctx, {"until": "\n\n"})
+        return cont_request
+
+    def doc_to_target(self, doc):
+        return doc["answer"]
+
+    def process_results(self, doc, results):
+        gold = doc["answer"]
+
+        acc = 1.0 if results[0].strip() == gold else 0.0
+
+        return {
+            "acc": acc,
+        }
+
+    def higher_is_better(self):
+        return {
+            "acc": True,
+        }
+
+    def aggregation(self):
+        return {
+            "acc": mean,
+        }
+
+
+class StockMovement(MultipleChoiceTask):
+    VERSION = 1
+    # DATASET_PATH = "chancefocus/flare-fpb"
+    DATASET_NAME = None
+
+    def has_training_docs(self):
+        return True
+
+    def has_validation_docs(self):
+        return True
+
+    def has_test_docs(self):
+        return True
+
+    def training_docs(self):
+        return self.dataset["train"]
+
+    def validation_docs(self):
+        return self.dataset["validation"]
+
+    def test_docs(self):
+        return self.dataset["test"]
+
+    def should_decontaminate(self):
+         return True
+
+    def doc_to_decontamination_query(self, doc):
+         return doc["text"]
+
+    def doc_to_text(self, doc):
+        # TODO: Format the query prompt portion of the document example.
+        return doc["query"]
+
+    def process_results(self, doc, results):
+        gold = doc["gold"]
+
+        acc = 1.0 if np.argmax(results) == gold else 0.0
+        completion_len = np.array([float(len(i)) for i in doc["choices"]])
+        acc_norm = 1.0 if np.argmax(results / completion_len) == gold else 0.0
+
+        return {
+            "acc": acc,
+            "acc_norm": acc_norm,
+            "mcc": (np.argmax(results), gold),
+        }
+
+    def higher_is_better(self):
+        return {
+            "acc": True,
+            "acc_norm": True,
+            "mcc": True,
+        }
+
+    def aggregation(self):
+        return {
+            "acc": mean,
+            "acc_norm": mean,
+            "mcc": matthews_corrcoef,
+        }
+
+
+class StockMovementBigData(StockMovement):
+    DATASET_PATH = "chancefocus/flare-sm-bigdata"
+
+class StockMovementACL(StockMovement):
+    DATASET_PATH = "chancefocus/flare-sm-acl"
+
+class StockMovementCIKM(StockMovement):
+    DATASET_PATH = "chancefocus/flare-sm-cikm"
+
+SM_TASKS = {
+    "flare_sm_bigdata": StockMovementBigData,
+    "flare_sm_acl": StockMovementACL,
+    "flare_sm_cikm": StockMovementCIKM,
+}
