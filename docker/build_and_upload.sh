6f39cf62ee548dfb66bd7ba5a1b6748a4d7bef3d
Weiguang Han <hanweiguang1@gmail.com>
2023-08-10 22:24:19 +0800

    update docker

--- docker/build_and_upload.sh
+++ docker/build_and_upload.sh
@@ -2,7 +2,10 @@ export https_proxy=...
 export http_proxy=...
 export all_proxy=...
 docker_user=...
+tag=$(date +%Y%m%d)
 
-docker build --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg all_proxy=$all_proxy -t pixiu:$(date +%Y%m%d) -f DOCKERFILE .
-docker tag pixiu:$(date +%Y%m%d) $docker_user/pixiu:$(date +%Y%m%d)
-docker push $docker_user/pixiu:$(date +%Y%m%d)
+docker build --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg all_proxy=$all_proxy -t pixiu:$tag -f DOCKERFILE .
+docker tag pixiu:$tag $docker_user/pixiu:$tag
+docker push $docker_user/pixiu:$tag
+docker tag pixiu:$tag $docker_user/pixiu:latest
+docker push $docker_user/pixiu:latest
