a702158c690e8ed01163c970bfd240212d550d67
Weiguang Han <hanweiguang1@gmail.com>
2023-08-15 13:52:35 +0000

    modify shell

--- scripts/run_evaluation.sh
+++ scripts/run_evaluation.sh
@@ -1,14 +1,15 @@
-pixiu_path='/root/PIXIU'
+pixiu_path='/workspace/PIXIU'
 export PYTHONPATH="$pixiu_path/src:$pixiu_path/src/financial-evaluation:$pixiu_path/src/metrics/BARTScore"
 echo $PYTHONPATH
 export CUDA_VISIBLE_DEVICES="0"
 
 python src/eval.py \
-    --model hf-causal-llama \
+    --model hf-causal-experimental \
     --tasks flare_edtsum,flare_ectsum \
-    --model_args use_accelerate=True,pretrained=chancefocus/finma-7b-full,tokenizer=chancefocus/finma-7b-full,use_fast=False,max_gen_toks=1024,dtype=float16 \
+    --model_args use_accelerate=True,pretrained=EleutherAI/gpt-neox-20b,tokenizer=EleutherAI/gpt-neox-20b,use_fast=True,max_gen_toks=1024,dtype=float16 \
     --no_cache \
     --batch_size 4 \
-    --model_prompt 'finma_prompt' \
+    --model_prompt 'no_prompt' \
     --num_fewshot 0 \
+    --limit 200 \
     --write_out 
