0115639b5772b5767fdba8f79475d0381add7ce1
czy <940250091@qq.com>
2023-12-14 14:51:43 +0000

    flare

--- scripts/run_evaluation.sh
+++ scripts/run_evaluation.sh
@@ -1,11 +1,11 @@
-pixiu_path='/data/chenzhengyu/projects/PIXIU_PR/PIXIU_flare_pr/PIXIU'
+pixiu_path='/root/PIXIU'
 export PYTHONPATH="$pixiu_path/src:$pixiu_path/src/financial-evaluation:$pixiu_path/src/metrics/BARTScore"
 echo $PYTHONPATH
 export CUDA_VISIBLE_DEVICES="0"
 
 python src/eval.py \
     --model hf-causal-llama \
-    --tasks flare_fp \
+    --tasks flare_fpb \
     --model_args use_accelerate=True,pretrained=chancefocus/finma-7b-full,tokenizer=chancefocus/finma-7b-full,use_fast=False,max_gen_toks=1024,dtype=float16 \
     --no_cache \
     --batch_size 4 \
