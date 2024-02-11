b90681a88aa8d86300d63d06e76596a4c3040c86
Me1oyy <940250091@qq.com>
2024-02-11 14:56:38 +0800

    PIXIU2.0_PR_1

--- scripts/run_evaluation.sh
+++ scripts/run_evaluation.sh
@@ -1,12 +1,12 @@
-pixiu_path='/root/PIXIU'
+pixiu_path='/data/chenzhengyu/projects/PIXIU2.0_PR/PIXIU'
 export PYTHONPATH="$pixiu_path/src:$pixiu_path/src/financial-evaluation:$pixiu_path/src/metrics/BARTScore"
 echo $PYTHONPATH
 export CUDA_VISIBLE_DEVICES="0"
 
 python src/eval.py \
-    --model hf-causal-llama \
-    --tasks flare_edtsum,flare_ectsum \
-    --model_args use_accelerate=True,pretrained=chancefocus/finma-7b-full,tokenizer=chancefocus/finma-7b-full,use_fast=False,max_gen_toks=1024,dtype=float16 \
+    --model hf-causal-vllm \
+    --tasks  \
+    --model_args use_accelerate=True,pretrained=/data/chenzhengyu/models/base_models/PIXIU/finma-7b-full-web,tokenizer=/data/chenzhengyu/models/base_models/PIXIU/finma-7b-full-web,use_fast=False,max_gen_toks=1024,dtype=float16 \
     --no_cache \
     --batch_size 4 \
     --model_prompt 'finma_prompt' \
