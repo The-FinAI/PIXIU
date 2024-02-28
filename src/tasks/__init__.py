0e2498c8bf52cae3a15de5afae21f84b46ff4341
Me1oyy <940250091@qq.com>
2024-02-28 20:33:15 +0800

    finterm and MT

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -47,6 +47,10 @@ TASK_REGISTRY = {
     "flare_sm_cikm": flare.StockMovementCIKM,
     "flare_en_fintern": flare.FINTERM,
     "flare_en_acronym": flare.ACRONYM,
+    "flare_mt_idiom": flare.MT_idiom,
+    "flare_mt_stencepair": flare.MT_stencepair,
+    "flare_mt_tense": flare.MT_tense,
+    "flare_mt_zpt": flare.MT_zpt,
     **flare.SM_TASKS,
 }
 
